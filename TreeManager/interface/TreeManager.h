cc88b4379f982763cb8ba5d02473e93e120e5a05
duarte <duarte@ifca.unican.es>
2011-10-30 19:09:01 +0100

    Added TreeManager package
    Ignore-this: 5e10632d95db208deef9a9a49894b7c8
    
    darcs-hash:20111030180901-e973b-91d0f8cc0b16ed82b7036b93237a5cf79634e90c.gz

---
+++ TreeManager/interface/TreeManager.h
@@ -0,0 +1,53 @@
+// -*- C++ -*-
+//
+// Package:    TreeManager
+// Class:      TreeManager
+// 
+/**\class  TreeManager.h
+
+ Description: This class manage the output format of the root files.
+              This is an abstract class and the concrete classes
+	      will content the data members available on a file, taking
+	      care of the branches initialization, etc... The class is used 
+	      to encapsulate the different types of Trees (MiniTrees, Latinos
+	      and TESCO) taking the responsability just for this class to known how is
+	      the content of a ROOT file. Some concrete type of this class is
+	      going to be as datamember in the Analysis (client), the methods used 
+	      by the client has to be defined here (as pure virtual methods). So,
+	      in order to unify all the kind of Trees here has to be defined the
+	      methods used to extract info from the file, and in the concrete class
+	      the concrete implementation depending of the Tree kind.
+
+ Implementation: Abstract class used by the Analysis client. Just the initialization
+              of the tree and the definition of the methods which has to be implemented
+	      in the concrete classes
+*/
+//
+// Original Author: Jordi Duarte Campderros  
+//         Created:  Sun Oct  30 12:20:31 CET 2011
+// 
+// jordi.duarte.campderros@cern.ch
+//
+//
+#ifndef TREEMANAGER_H
+#define TREEMANAGER_H
+
+#include "TROOT.h"
+#include "TChain.h"
+
+class TreeManager
+{
+	public:
+		TreeManager() : fChain(0) { }
+		virtual ~TreeManager() { };
+
+		virtual void Init(TTree * tree) = 0;
+
+	protected:
+		TTree * fChain;
+
+	
+	ClassDef(TreeManager,0);
+};
+
+#endif
---
+++ TreeManager/interface/TreeTypes.h
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//
+// Package:    TreeManager
+// Class:      ---
+// 
+/**\class 
+
+ Description: Enum types
+
+ Implementation: ---
+*/
+//
+// Original Author: Jordi Duarte Campderros  
+//         Created:  Sun Oct  30 18:45:11 CET 2011
+// 
+// jordi.duarte.campderros@cern.ch
+//
+//
+
+#ifndef TREETYPES_H
+#define TREETYPES_H
+
+enum treeTypes
+{
+	MiniTrees,   // 
+	Latinos,     // - Same flavour, just minor changes
+	TESCO
+};
+
+#endif
