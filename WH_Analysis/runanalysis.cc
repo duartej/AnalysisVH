3e83b9f8ab5aaaada1a2e23d6b47c89f7ec3d11b
duarte <duarte@ifca.unican.es>
2011-11-07 16:12:26 +0100

    Some modifications in the executable (runanalysis).. I don't know if they are worth it
    Ignore-this: 24aa24acfe86fe648550eb84ae21d000
    
    darcs-hash:20111107151226-e973b-2be10de490503698eb123f7289fa922905c701f1.gz

--- WH_Analysis/runanalysis.cc
+++ WH_Analysis/runanalysis.cc
@@ -36,6 +36,136 @@
 #include "TStopwatch.h"
 #endif
 
+// Storing the datafiles
+void AddDataFiles(const std::vector<TString> & files, 
+		std::vector<TString> & datafiles)
+{
+	datafiles.reserve( datafiles.size() + files.size() );
+	datafiles.insert( datafiles.end(), files.begin(), files.end() );
+}
+
+void PrintDataFiles(const std::vector<TString> & dataFiles)
+{
+	for(unsigned int i = 0; i < dataFiles.size(); ++i)
+	{
+		std::cout << "* " << dataFiles[i] << std::endl;
+	}
+}
+
+// Giving a dataset extract all the files and stores them in a file
+const std::vector<TString> extractdatafiles(TString dataName = "HW160" )
+{
+	///////////////////////////////
+	// INPUT DATA SAMPLE
+	// 
+	// 1) Load DatasetManager
+	DatasetManager* dm = 0;
+	
+	std::vector<TString> datafiles;
+	// 2) Asign the files 
+	if(dataName.Contains("LocalWH")) 
+	{
+		datafiles.push_back( "/hadoop/PrivateProduction/WH_2l_42X/Tree_WH_2l_42X.root" );
+	}
+	else if(dataName.Contains("Data")) 
+	{
+		std::vector<TString> data1= 
+			DatasetManager::GetRealDataFiles("Data7TeVRun2011A_newJEC", 
+					"Tree_DoubleMu_May10_Skim2LPt1010_215.3");
+		AddDataFiles(data1,datafiles);
+                
+		std::vector<TString> data2= 
+             		  DatasetManager::GetRealDataFiles("Data7TeVRun2011A_newJEC", 
+             				       "Tree_DoubleMuV4_Skim2LPt1010_927.9");
+		AddDataFiles(data1,datafiles);
+                
+		std::vector<TString> data3= 
+             		  DatasetManager::GetRealDataFiles("Data7TeVRun2011A_newJEC", 
+             				  "Tree_DoubleMuAug5_Skim2LPt1010_334.4");
+		AddDataFiles(data1,datafiles);
+             	
+		std::vector<TString> data4= 
+             		  DatasetManager::GetRealDataFiles("Data7TeVRun2011A_newJEC", 
+				  "Tree_DoubleMuV6_Skim2LPt1010_662.9");
+		AddDataFiles(data1,datafiles);
+	
+		std::cout << ">> List of Real Data Files:" << std::endl;
+		
+		PrintDataFiles(datafiles);
+	}
+	else 
+	{
+		TString folder("Summer11");
+		TString skim("Skim2LPt1010");
+		//TString folder("Spring11Latinos");
+		//TString skim("/");
+
+		if (dataName.Contains("WH")) 
+		{
+			folder = "Summer11";
+			skim = "HWW";
+			dataName.Replace(0,2, "WHToWW2L");
+		}
+		
+		//Use DatasetManager
+		dm = new DatasetManager(folder, skim);
+		
+		dm->LoadDataset(dataName);  // Load information about a given dataset
+		AddDataFiles(dm->GetFiles(),datafiles); //Find files
+		
+#ifdef DEBUG
+		std::cout << std::endl;
+		std::cout << "      x-section = " << dm->GetCrossSection()      << std::endl;
+		//std::cout << "     luminosity = " << G_Event_Lumi               << std::endl; --> NO DEFINIDA
+		std::cout << "        nevents = " << dm->GetEventsInTheSample() << std::endl;
+		std::cout << " base file name = " << dm->GetBaseFileName()      << std::endl;
+		std::cout << std::endl;
+		dm->Dump();
+#endif
+		
+		if(datafiles.size() == 0) 
+		{
+			std::cerr << "ERROR: Could not find dataset " 
+				<< dataName << " with DatasetManager for folder " 
+				<< folder << "!!!" << std::endl;
+			std::cerr << "       Exiting!" << std::endl;      
+			exit(-1);
+		}
+
+		// Freeing memory
+		delete dm; 
+		dm = 0;
+	}
+
+	// Persistency
+	std::string filename( dataName+"_datanames.dn" );
+	std::ofstream of( filename.c_str() );
+	if( ! of.is_open() )
+	{
+		std::cerr << "ERROR: Imposible to open the file '" << filename 
+			<< "' to store file names information" << std::endl;
+		exit(-1);
+	}
+	// TreeType
+	treeTypes treeType;
+	if( datafiles[0].Contains("TESCO") )
+	{
+		treeType = TESCO;
+	}
+	else 
+	{
+		treeType = MiniTrees;
+	}
+
+	of << treeType << std::endl;
+	for(std::vector<TString>::iterator it = datafiles.begin(); it != datafiles.end(); ++it)
+	{
+		of << it->Data() << std::endl;
+	}
+	of.close();
+
+	return datafiles;	
+}
 
 // Overloaded function to extract the file names from a previous stored file
 // It will catch the files inside a subset
@@ -52,11 +182,15 @@ std::pair<treeTypes,std::vector<TString> > extractdatafiles( const char * dataNa
 	std::ifstream inputf( filename );
 	if( ! inputf.is_open() )
 	{
-		std::cerr << "ERROR: Imposible to open the file '" << filename 
+		// Call the extractdatafiles overloaded to create the filename
+		std::vector<TString> dum = extractdatafiles(TString(dataName));
+		return extractdatafiles(dataName);
+		// And call again this function
+	/*	std::cerr << "ERROR: Imposible to open the file '" << filename 
 			<< "' to retrieve file names information.\n"
 			<< "Run this code previously with the '-t local' option to create the file"
 			<< std::endl;
-		exit(-1);
+		exit(-1);*/
 	}
 	
 	// TreeType
@@ -84,7 +218,6 @@ std::pair<treeTypes,std::vector<TString> > extractdatafiles( const char * dataNa
 
 	return std::pair<treeTypes,std::vector<TString> >(en_treeType,datafiles);
 }
-
 
 
 // From a config file, extract the parameters needed
@@ -150,11 +283,40 @@ InputParameters * setparameters(const std::vector<TString> & datafiles, const TS
 
 int main(int argc, char *argv[])
 {
-	// FIXME: Argumento de entrada: tipo de muestra
-	//                              path al header de analisis
-	//                              
-	const char * dataName = "WH160";
+	const char * dataName; // = "WH160";
 	const char * cfgfile = "analisiswh_mmm.ip";
+	//Parsing input options
+	if(argc == 1)
+	{
+		std::cout << "usage: runanalysis dataname [options]" << std::endl;
+		std::cout << "" << std::endl;
+		std::cout << "Options:" << std::endl;
+		std::cout << "    -c configuration file " << std::endl;
+		std::cout << "" << std::endl;
+		std::cout << "List of known dataname:" << std::endl;
+		std::cout << "    Higgs:             WH# (#: Higgs Mass hypothesis)" << std::endl;
+		std::cout << "    Z + Jets Madgraph: ZJets_Madgraph" << std::endl;
+		std::cout << "    Z + Jets Powheg:   DYee DYmumu Dytautau Zee_Powheg Zmumu_Powheg Ztautau_Powheg" << std::endl;
+		std::cout << "    Zbb + Jets:        Zbb" << std::endl;
+		std::cout << "    Other backgrounds: WZ ZZ WW TTbar_Madgraph WJetas_Madgraph TW TbarW" << std::endl;
+		return -1;
+	}
+	else if( argc == 2)
+	{
+		dataName = argv[1];
+	}
+	else
+	{
+		//Argumet 1 must be a valid input fileName
+		dataName = argv[1];
+		for(int i = 2; i < argc; i++) 
+		{
+			if( strcmp(argv[i],"-c") == 0 )
+			{
+				cfgfile = argv[i+1];
+			}
+		}
+	}
 #ifdef TIMERS
 	TStopwatch timer;
 	timer.Start();
