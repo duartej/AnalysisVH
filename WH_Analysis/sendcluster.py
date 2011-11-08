5958fb8218357a6cfaa3b8d767c49804f39e54d4
duarte <duarte@ifca.unican.es>
2011-11-08 20:23:37 +0100

    Utility sendcluster almost finished... Still need some fixes with respect the paths
    Ignore-this: 7e9410a58b6fbd997d65d3dc46ab139d
    
    darcs-hash:20111108192337-e973b-3447b605b2d210fbaa3271670160b98959fb31f9.gz

--- WH_Analysis/sendcluster.py
+++ WH_Analysis/sendcluster.py
@@ -9,7 +9,6 @@ class clustermanager(object):
 	Class to manage the sending and harvesting
 	of jobs in the cluster
 	"""
-	precompile = False
 	def __init__(self,status,**keywords):
 		"""
 		Initializes
@@ -17,8 +16,11 @@ class clustermanager(object):
 		import os
 		import sys
 		
-		validkeys = [ 'dataname', 'cfgfile', 'njobs', 'precompile', 'packagepath' ]
+		validkeys = [ 'dataname', 'cfgfile', 'njobs', 'precompile', 'packagepath',\
+				'workingdir' ]
 		self.pkgpath = "."
+		self.precompile = False
+		self.outputfiles= {}
 		for key,value in keywords.iteritems():
 			if key not in validkeys:
 				message = "\nclustermanager: ERROR Not a valid argument '"+key+\
@@ -51,6 +53,8 @@ class clustermanager(object):
 							+value+"' do not contain the header interface/AnalysisBuilder.h\n"
 					sys.exit( message )
 				self.pkgpath = value
+			elif key == 'workingdir':
+				self.cwd = value
 		
 		self.status = status
 		
@@ -82,6 +86,70 @@ class clustermanager(object):
 			# Submit the jobs
 			self.submit()
 
+		if self.status == "harvest":
+			# Going to the working directory
+			os.chdir(self.cwd)
+			# And update with the last used datamembers
+			self.retrieve()
+
+			foundoutfiles = []
+			for id,bashscript in self.jobsid:
+				foundoutfiles.append( self.checkjob(id) )
+			
+			# If we have all the outputfiles we can gathering
+			if foundoutfiles == self.outputfiles:
+				self.gatherfiles()
+
+	def gatherfiles(self):
+		"""Gather all the outputfiles in one
+		"""
+		from subprocess import Popen,PIPE
+		import os
+		
+		print "Joining all the files in one: "
+		# FIXME: Only there are 1 file, not needed the hadd
+		finalfile = os.path.join("Results",self.dataname)
+		command = [ 'hadd', finalfile ]
+		for f in self.outputfiles.itervalues():
+			command.append( f )
+		p = Popen( command ,stdout=PIPE,stderr=PIPE ).communicate()
+		if p[1] != "":
+			message = "\nclustermanager.gatherfiles: ERROR Something wrong with hadd\n"
+			message += p[1]+"\n"
+			sys.exit(message)
+
+		else:
+			print "Created "+finalfile
+			print "========= Process Completed ========="
+			#FIXME: REMOVE THE OLD FILES
+
+
+
+	def checkjob(self, id):
+		"""Check the status of a job
+		"""
+		from subprocess import Popen,PIPE
+		import os
+		
+		print "Checking the job status:"
+		command = [ 'qstat','-j',id ]
+		p = Popen( command ,stdout=PIPE,stderr=PIPE ).communicate()
+		# The job is done and disappeared from the scheluder
+		if p[1] != "":
+			# Checking if the outputfiles are there
+			if not os.path.exists(self.outputfiles[self.jobsidID[id]]):
+				message = "\nclustermanager.checkjob: Something went wrong in the cluster:\n"
+				message += "The job '"+id+"' is already finish but there is no output root file '"
+				message += self.outputfiles[self.jobsidID[id]]+"'\n"
+				message += "Check the cluster output file\n"
+				sys.exit(message)
+
+			# Gathering the file outputs in order to add
+			return self.outputfiles[self.jobsidID[id]]
+		else:
+			# Still in the cluster
+			print "=== Job '"+id+"' is still in the cluster"
+
 
 	def submit(self):
 		"""Submit the jobs
@@ -104,32 +172,73 @@ class clustermanager(object):
 		# Moving to that dir
 		os.chdir( executedir )
 		# Extract the number of events per job
-		jobsid = []
+		jobnames = []
 		for i,evttuple in self.jobidevt:
 			# Splitting the config file in the number of jobs
-			iconfig = self.createconfig(i,evttuple)
+			iconfigname = self.createconfig(i,evttuple)
 			# Creating the bash script to send the job
-#			if self.precompile:
-#				# Send the previous job to compile and update the code
-#				# create job to compile
-#				jobnames = createbash(os.path.join(self.pkgpath,'datamanagercreator'),\
-#						self.dataname,self.cfgfile)
-#			else:
-#				jobnames = createbash(os.path.join(self.pkgpath,'runanalysis'),\
-#						self.dataname,self.cfgfile)
-#			# sending the jobs
-#			for job in jobnames:
-#				jobsid.append( sendjob(job) )
+			if self.precompile:
+				# Send the previous job to compile and update the code
+				# create job to compile
+				jobnames.append( self.createbash(os.path.join(self.pkgpath,'datamanagercreator'),\
+						iconfigname,i) )
+				break
+			else:
+				jobnames.append( self.createbash(os.path.join(self.pkgpath,'runanalysis'),\
+						iconfigname,i) )
+		# sending the jobs
+		jobsid = []
+		# Storing the cluster job id with the number of job (ID)
+		jobsidID= {}
+		for bashname,job in jobnames:
+			jobsid.append( self.sendjob(bashname) )
+			jobsidID[jobsid[-1][0]] = job
+
+		self.jobsid = jobsid
+		self.jobsidID = jobsidID
+
+		#persistency
+		self.store()
 		
+		# Returning initial directory
 		os.chdir(launchDir)
-		print ""
-		print "The jobs have been sended: "
-		for i in jobsid:
-			print i,
-		print ""
-		print "You can observe the status using 'qstat -u "+os.getenv("USER")+"'"
-		print "Good Luck!"
-		print ""
+	
+	def retrieve(self):
+		"""Retrieve and populates the class from a file
+		"""
+		import shelve
+		import sys
+
+		d = shelve.open(".storedmanager")
+		if not d.has_key("storedmanager"):
+			message = "\nclustermanager.retrieve: ERROR Not found the" \
+					+" class stored in .storedmanager file"
+			sys.exit(message)
+
+		copyself = d["storedmanager"]
+		
+		# Putting the datamembers: FIXME: If you want all the datanames
+		# do it with the __dict__ and __setattr__ methods
+		self.dataname    = copyself.dataname
+		self.jobsid      = copyself.jobsid
+		self.outputfiles = copyself.outputfiles
+		self.njobs       = copyself.njobs
+		self.jobsidID    = copyself.jobsidID
+		
+		d.close()
+
+
+	def store(self):
+		"""Store the relevant info after the submission
+		in order to be retrieved at another stage
+		"""
+		import shelve
+
+		d = shelve.open(".storedmanager",writeback=True)
+		d["storedmanager"] = self
+
+		d.close()
+
 		
 	def getevents(self,filedatanames):
 		"""Extract the number of events of a dataname
@@ -188,38 +297,55 @@ class clustermanager(object):
 		f.writelines( newlines )
 		f.close()
 
+		return newcfgname
+	
+	def createbash(self,executable,cfgname,jobnumber):
+		"""Create the bash scripts to be sended to the cluster
+		Return a list of (bash script names (str),jobnumber)
+		"""
+		import os
+		import stat
+		
+		outputname = os.path.join("Results",self.dataname+"_"+str(jobnumber)+".root")
+		self.outputfiles[jobnumber] = outputname
 
-
-def sendjob(bashscript):
-	"""
-	Send to the cluster the bash script input
-	Return the job id 
-	"""
-
-
-
-
-def createbash(executable,dataname,cfgfile,jobsNumber):
-	"""
-	Create the bash scripts to be sended to the cluster
-	Return a list of bash script names (str)
-	"""
-	import os
-	import stat
-
-	lines  = "#!\\bin\\bash\n"
-	lines += "\n# Script created automatically by sendcluster.py utility\n"
-	lines += "\n\n"+executable+" "+dataname+" -c "+cfgfile+"\n"
-
-	for i in xrange(jobsNumber):
-		filename = dataname+"_"+str(i)+".sh"
+		lines  = "#!\\bin\\bash\n"
+		lines += "\n# Script created automatically by sendcluster.py utility\n"
+		lines += "\nmkdir -p Results\n"
+		lines += executable+" "+self.dataname+" -c "+cfgname+" -o "+outputname+"\n"
+	
+		filename = self.dataname+"_"+str(jobnumber)+".sh"
 		f = open(filename,"w")
 		f.writelines(lines)
 		f.close()
-
 		os.chmod(filename,stat.S_IRWXU+stat.S_IRGRP+stat.S_IXGRP+stat.S_IXOTH)
+		
+		return (filename,jobnumber)
 
-	return []
+	def sendjob(self,bashscript):
+	   	"""Send to the cluster the bash script input
+		Return the job id 
+		"""
+		from subprocess import Popen,PIPE
+		
+		# OJO NO HE PUESTO REQUERIMIENTOS DE MEMORIA, CPU...
+		print "Sending to cluster:"+bashscript
+		command = [ 'qsub','-V','-cwd','-S','/bin/bash', \
+				'-P','l.gaes','-l','h_rt=00:10:00',bashscript ]
+		p = Popen( command ,stdout=PIPE,stderr=PIPE ).communicate()
+		if p[1] != "":
+			message = "\nclustermanager: ERROR from 'qsub':\n"
+			message += p[1]+"\n"
+			sys.exit(message)
+		
+		#Extract id
+		id = None
+		outputline = p[0].split(' ')
+		for i in outputline:
+			if i.isdigit():
+				id = i
+		return (id,bashscript)
+
 
 
 from optparse import OptionParser
@@ -241,6 +367,7 @@ if __name__ == '__main__':
 	parser = OptionParser()
 	parser.set_defaults(shouldCompile=False,jobsNumber=10)
 	parser.add_option( '-a', '--action', action='store', type='string', dest='action', help="Action to proceed: submit, harvest" )
+	parser.add_option( '--wd', action='store', type='string', dest='workingdir', help="Working directory used with the '-a harvest' option")
 	parser.add_option( '-d', '--dataname',  action='store', type='string', dest='dataname', help='Name of the data (see runanalysis)' )
 	parser.add_option( '-j', '--jobs',  action='store', type='int',    dest='jobsNumber', help='Number of jobs' )
 	parser.add_option( '-c', '--cfg' ,  action='store', type='string', dest='config', help='name of the config file (absolute path)' )
@@ -252,34 +379,41 @@ if __name__ == '__main__':
 
 	#Archivo de configuracion obligatorio:
 	configabspath = ''
-	if opt.config is None:
-		message = "\nsendcluster: ERROR the '-c' option is mandatory.\n"
-		sys.exit( message )
-	else:
-		#Checking is a file and can be find it
-		if not os.path.exists(opt.config):
-			message = "\nsendcluster: ERROR Not found '"+opt.config+"'.\n"
-			sys.exit( message )
-		configabspath = os.path.abspath(opt.config)
-	#Dataname obligatorio:
-	if opt.dataname is None:
-		message = "\nsendcluster: ERROR the '-d' option is mandatory.\n"
-		sys.exit( message )
-
 	# Instanciation of the class manager depending the action to be done
 	if opt.action is None:
 		message = "\nsendcluster: ERROR the '-a' option is mandatory: 'submit' 'harvest'\n"
 		sys.exit( message )
-
-	manager = None
+	
 	if opt.action == 'submit':
-		manager = clustermanager('submit',dataname=opt.dataname,\
-				cfgfile=configabspath,njobs=opt.jobsNumber)
+		if opt.config is None:
+			message = "\nsendcluster: ERROR the '-c' option is mandatory.\n"
+			sys.exit( message )
+		else:
+			#Checking is a file and can be find it
+			if not os.path.exists(opt.config):
+				message = "\nsendcluster: ERROR Not found '"+opt.config+"'.\n"
+				sys.exit( message )
+			configabspath = os.path.abspath(opt.config)
+		#Dataname obligatorio:
+		if opt.dataname is None:
+			message = "\nsendcluster: ERROR the '-d' option is mandatory.\n"
+			sys.exit( message )
+		# Instantiate and submit
+		manager = None
+		if opt.action == 'submit':
+			manager = clustermanager('submit',dataname=opt.dataname,\
+					cfgfile=configabspath,njobs=opt.jobsNumber)
+	elif opt.action == 'harvest':
+		if opt.workingdir is None:
+			message = "\nsendcluster: ERROR the '--cw' option is mandatory.\n"
+			sys.exit( message )
 
-		#manager.submit()
+		if not os.path.exists(opt.workingdir):
+			message = "\nsendcluster: ERROR the working path "+opt.workingdir \
+					+"' does not exists\n"
+			sys.exit( message )
 
-
-
-
+		manager = clustermanager("harvest",workingdir=opt.workingdir)
+	
 
 
