5560861d09121b54b6a2062c6d03d05b792078c8
duarte <duarte@ifca.unican.es>
2011-11-09 16:09:26 +0100

    extractEvents now can evaluate the number of events before and after the processing
    Ignore-this: d073ef6923e93c72f3f2f31eceea211e
    
    darcs-hash:20111109150926-e973b-0dd06a409f0b0ce7856926d45f4dd4320dddbf40.gz

--- Utils/extractEvents.cc
+++ Utils/extractEvents.cc
@@ -13,22 +13,35 @@
 
 #include "TFile.h"
 #include "TChain.h"
+#include "TH1D.h"
 
 #ifndef __CINT__
 int main( int argc, const char * argv[])
 {
+	bool histofile = false;
+
 	if( argc == 1)
 	{
-		std::cerr << "usage: extractEvents filename*" << std::endl;
+		std::cerr << "usage: extractEvents filename* [options]" << std::endl;
 		std::cerr << "" << std::endl;
 		std::cerr << "filename* could be a wildcard pattern" << std::endl;
+		std::cerr << "Options:" << std::endl;
+		std::cerr << "    -h  flag to use final datafiles (already processed) " << std::endl;
 		exit(-1);
 	}
 	
 	std::vector<std::string> files;
 	for(int i = 1; i < argc; ++i)
 	{
-		files.push_back( argv[i] );
+		if( strcmp(argv[i],"-h") == 0 )
+		{
+			histofile = true;
+			continue;
+		}
+		else
+		{
+			files.push_back( argv[i] );
+		}
 	}
 	// 
 	if( files.size() == 0 )
@@ -36,7 +49,21 @@ int main( int argc, const char * argv[])
 		std::cerr << "ERROR: you must enter a valid file name" << std::endl;
 		exit(-1);
 	}
+	
+	// Processed files, just look into the histograms
+	if( histofile )
+	{
+		TFile * f = new TFile(files[0].c_str());
+		TH1D *  h = (TH1D*)f->Get("fHProcess");
+		std::cout << h->GetEntries() << std::endl;
 
+		f->Close();
+		delete f;
+
+		return 0;
+	}
+
+	//Else look a chain
 	TChain * chain = new TChain("Tree");
 
 	for(unsigned int i = 0; i < files.size(); ++i)
