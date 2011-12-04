974836aa4886ff8dcf5fad4115e3cc913255cb4a
duarte <duarte@gridui.ifca.es>
2011-12-04 14:18:50 +0100

    In the middle of some changes: get the data files given the run period, ...
    Ignore-this: feeaca03a3f14293ea7b7ab43d109cf2
    
    darcs-hash:20111204131850-5c877-5b29248caa422b9c1f8d667e46ec38e347c007b2.gz

--- WH_Analysis/datamanagercreator.cc
+++ WH_Analysis/datamanagercreator.cc
@@ -37,12 +37,99 @@ void AddDataFiles(const std::vector<TString> & files,
 	datafiles.insert( datafiles.end(), files.begin(), files.end() );
 }
 
+std::pair<std::string,std::vector<std::string> > getdatapathfile(const char * runperiod,const char * finalstate)
+{
+	if( finalstate == 0 )
+	{
+		std::cerr << "\033[1;31mgetdatapathfile ERROR\033[1;m Need to kwown the final"
+			<< " state to extract the data filenames."
+			<< " See \033[1;37datamanagercreator -h\033[1;m" << std::endl;
+		exit(-1);
+	}
+	std::string production = "LatinosSkim";
+	std::string runpath;
+	std::vector<std::string> filenames;
+
+	if( strcmp(runperiod,"2011A") == 0 )
+	{
+		runpath = "../"+production+"/Data7TeVRun2011A_newJEC_Reload";
+		// O Quizas este --> runpath = "../"+production+"/Data7TeVRun2011A";
+		if( strcmp(finalstate,"mmm") == 0 )
+		{
+			// DoubleMuon
+			filenames.push_back("Tree_DoubleMuMay10_210.5");
+			filenames.push_back("Tree_DoubleMuV4_927.9");
+			filenames.push_back("Tree_DoubleMuAug5_334.4");
+			filenames.push_back("Tree_DoubleMuV6_662.9");
+		}
+		else if( strcmp(finalstate,"eee") == 0 )
+		{
+			filenames.push_back("Tree_DoubleElectronMay10_210.5");
+			filenames.push_back("Tree_DoubleElectronV4_927.9"); // OJO hay dos, lo hara el DM
+			filenames.push_back("Tree_DoubleElectronAug5_334.4"); 
+			filenames.push_back("Tree_DoubleElectronV6_662.9");
+		}
+		else if( strcmp(finalstate,"mme") == 0  ||
+			strcmp(finalstate,"eem") == 0 )
+		{
+			filenames.push_back("Tree_MuEGMay10_210.5");
+			filenames.push_back("Tree_MuEGV4_927.9"); // OJO hay dos, lo hara el DM
+			filenames.push_back("Tree_MuEGAug5_334.4"); 
+			filenames.push_back("Tree_MuEGV6_662.9");
+		}
+		else
+		{
+			std::cerr << "\033[1;31mgetdatapathfile ERROR\033[1;m Not recognized"
+				" finalstate '"  << finalstate << "'"
+				<< " See \033[1;37datamanagercreator -h\033[1;m" << std::endl;
+			exit(-1);
+		}
+	}
+	else if( strcmp(runperiod,"2011B") == 0 )
+	{
+		runpath = "../"+production+"Data7TeVRun2011B";
+		if( strcmp(finalstate,"mmm") == 0 )
+		{
+			// DoubleMuon
+			filenames.push_back("Tree_DoubleMuV1_Latinos_2509");
+		}
+		else if( strcmp(finalstate,"eee") == 0 )
+		{
+			filenames.push_back("Tree_DoubleElectronV1_Latinos_2509");
+		}
+		else if( strcmp(finalstate,"mme") == 0  ||
+			strcmp(finalstate,"eem") == 0 )
+		{
+			filenames.push_back("Tree_MuEGV1_Latinos_2509");
+		}
+		else
+		{
+			std::cerr << "\033[1;31mgetdatapathfile ERROR\033[1;m Not recognized"
+				" finalstate '"  << finalstate << "'"
+				<< " See \033[1;37datamanagercreator -h\033[1;m" << std::endl;
+			exit(-1);
+		}
+	}
+	else
+	{
+		std::cout << "\033[1;34mgetdatapathfile\033[1;m Not Implemented yet ('"
+			<< runperiod << "')" << std::endl;
+		exit(-3);
+	}
+
+	return std::pair<std::string,std::vector<std::string> >(runpath,filenames);
+}
+
+
 // Giving a dataset extract all the files
-const std::vector<TString> * extractdatafiles(TString dataName = "HW160" )
+const std::vector<TString> * extractdatafiles(TString dataName )
 {
 	double xsection = 0;
 	int    evtsample= 0;
 	bool storeXSE = false;
+
+	// Take the files which corresponds to each run period
+
 
 	///////////////////////////////
 	// INPUT DATA SAMPLE
@@ -162,89 +249,108 @@ const std::vector<TString> * extractdatafiles(TString dataName = "HW160" )
 	return datafiles;	
 }
 
-
-
-// Selector creation
-/*std::pair<std::string,treeTypes> createdatamanager(const std::vector<TString> & datafiles)
+void display_usage()
 {
-	// 3. Create the selector
-	TreeManagerCreator sc(datafiles[0]);
-	sc.MakeSimpleSelector();
-
-	return std::pair<std::string,treeTypes>(sc.getSelectorFilename(),sc.getTreeType());
-}*/
-
+	std::cout << "usage: datamanagercreator dataname [options]" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "    -r <2011A|2011B|2011>       Run period to extract datafiles" << std::endl;
+	std::cout << "    -f <mmm|mme|eee|eem>        Final state to be used in the data case" << std::endl;
+	std::cout << "    -h                          Displays this help message and exits " << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "List of known dataname:" << std::endl;
+	std::cout << "    Higgs:             WH# (#: Higgs Mass hypothesis)" << std::endl;
+	std::cout << "    Z + Jets Madgraph: ZJets_Madgraph" << std::endl;
+	std::cout << "    Z + Jets Powheg:   DYee DYmumu Dytautau Zee_Powheg Zmumu_Powheg Ztautau_Powheg" << std::endl;
+	std::cout << "    Zbb + Jets:        Zbb" << std::endl;
+	std::cout << "    Other backgrounds: WZ ZZ WW TTbar_Madgraph WJets_Madgraph TW TbarW" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-	const char * dataName; // = "WH160";
-	const char * analysispkgpath = ".";
-	bool justdatafiles = false;
-	const char * creadn = dataName;
+	const char * dataName  = 0; 
+	const char * runperiod = 0; 
+	const char * finalstate= 0;
+	
+	// Arguments used
+	std::set<int> usedargs;
 	
 	//Parsing input options
 	if(argc == 1)
 	{
-		std::cout << "usage: datamanagercreator dataname [options]" << std::endl;
-		std::cout << "" << std::endl;
-		std::cout << "Options:" << std::endl;
-		std::cout << "    -p <base_path_of_the_analysis_package> " << std::endl;
-		std::cout << "    -c Avoid the TreeManager concrete class creation " << std::endl;
-		std::cout << "       generating just the dataname.dn file" << std::endl;
-		std::cout << "    -a Using with the '-c' flag create all the known datanames" << std::endl;
-		std::cout << "" << std::endl;
-		std::cout << "List of known dataname:" << std::endl;
-		std::cout << "    Higgs:             WH# (#: Higgs Mass hypothesis)" << std::endl;
-		std::cout << "    Z + Jets Madgraph: ZJets_Madgraph" << std::endl;
-		std::cout << "    Z + Jets Powheg:   DYee DYmumu Dytautau Zee_Powheg Zmumu_Powheg Ztautau_Powheg" << std::endl;
-		std::cout << "    Zbb + Jets:        Zbb" << std::endl;
-		std::cout << "    Other backgrounds: WZ ZZ WW TTbar_Madgraph WJets_Madgraph TW TbarW" << std::endl;
+		display_usage();
 		return -1;
-	}
-	else if( argc == 2)
-	{
-		dataName = argv[1];
 	}
 	else
 	{
-		//Argument 1 must be a valid input fileName
-		dataName = argv[1];
-		for(int i = 2; i < argc; i++) 
+		for(int i = 1; i < argc; i++) 
 		{
-			if( strcmp(argv[i],"-p") == 0 )
+			if( strcmp(argv[i],"-h") == 0 )
 			{
-				analysispkgpath = argv[i+1];
-			}
-			
-		}
-		for(int i = 2; i < argc; i++) 
-		{
-			if( strcmp(argv[i],"-p") == 0 )
-			{
-				analysispkgpath = argv[i+1];
-			}
-			if( strcmp(argv[i],"-c") == 0 )
-			{
-				justdatafiles = true;
+				display_usage();
+				return 0;
 			}	
-			if( strcmp(argv[i],"-a") == 0 )
+			if( strcmp(argv[i],"-r") == 0 )
 			{
-				creadn = 0;
+				runperiod = argv[i+1];
+				if( strcmp(runperiod,"2011A") != 0 
+						&& strcmp(runperiod,"2011B") != 0 
+						&& strcmp(runperiod,"2011") != 0 )
+				{
+					std::cerr << "\033[1;31mdatamanager ERROR\033[1;m "
+						<< "Not a valid run period: '" << runperiod
+						<< "'" << std::endl;
+					display_usage();
+					return -1;
+				}
+				usedargs.insert(i);
+				usedargs.insert(i+1);
+				i++;
+			}
+			if( strcmp(argv[i],"-f") == 0 )
+			{
+				finalstate = argv[i+1];
+				usedargs.insert(i);
+				usedargs.insert(i+1);
+				i++;
 			}
 		}
+	}
+	
+	// Extracting the data name
+        for(int i=1; i < argc; i++)
+	{
+		if(usedargs.find(i) == usedargs.end())
+		{
+			dataName = argv[i];
+			break;
+		}
+	}
+	if( dataName == 0 )
+	{
+		std::cout << "\033[1,34mdatamanagercreator INFO\033[1;m All the known datasets" 
+			<< " will be created" << std::endl;
+		//std::cout << "List of known datasets:"
+	}
+	if( runperiod == 0 )
+	{
+		std::cerr << "\033[1;31mdatamanagercreator ERROR:\033[1;m The '-r' argument is mandatory!"
+			<< std::endl;
+		display_usage();
+		return -1;
 	}
 
 	std::set<std::string> knowndata;
 	// Signal
-        knowndata.insert("WH160");
         knowndata.insert("WH120");
         knowndata.insert("WH130");
-        knowndata.insert("WH140");
-        knowndata.insert("WH150");
-        knowndata.insert("WH170");
-        knowndata.insert("WH180");
-        knowndata.insert("WH190");
-        knowndata.insert("WH200");
+        //knowndata.insert("WH140");
+        //knowndata.insert("WH150");
+        //knowndata.insert("WH160");
+        //knowndata.insert("WH170");
+        //knowndata.insert("WH180");
+        //knowndata.insert("WH190");
+        //knowndata.insert("WH200");
 	// Z+jets
 	knowndata.insert("ZJets_Madgraph");
 	knowndata.insert("DYee");
@@ -272,61 +378,43 @@ int main(int argc, char *argv[])
 	{
 		std::cerr << "datamanagercreator: ERROR dataname '" << dataName << "'"
 			<< " not implemented! Exiting..." << std::endl;
-		exit(-1);
+		return -1;
 	}
 
 
 	//treeTypes dataType;
-	const std::vector<TString> * datafiles = extractdatafiles( TString(dataName) );
-	if( datafiles == 0 )
+	// Creating the .dn
+	bool wasprocessed = false;
+	for(std::set<std::string>::iterator it = knowndata.begin(); it != knowndata.end(); ++it)
 	{
-		std::cerr << "ERROR: Could not find dataset '" 
-			<< dataName << "' with DatasetManager!!! " << std::endl;
-		std::cerr << "       Exiting!" << std::endl;      
-		exit(-1);
-	}
-
-	if( justdatafiles )
-	{
-		if( creadn == 0 )
+		if( dataName != 0 )
 		{
-			// Creating all the .dn
-			for(std::set<std::string>::iterator it = knowndata.begin();
-					it != knowndata.end(); ++it)
+			if( strcmp(dataName, (*it).c_str() ) != 0 )
 			{
-				const std::vector<TString> * dummy = extractdatafiles( TString(*it) );
-				if( dummy != 0 )
-				{
-					delete dummy;
-					dummy = 0;
-				}
+				continue;
 			}
 		}
-		//return 0;
+		const std::vector<TString> * dummy = extractdatafiles( TString(*it) );
+		if( dummy != 0 )
+		{
+			delete dummy;
+			dummy = 0;
+		}
+		else
+		{
+			std::cerr << "\033[1;31mdatamanagercreator ERROR\033[1;m: Could not find dataset '" 
+				<< dataName << "' with DatasetManager!!! " << std::endl;
+			exit(-1);
+		}
+		wasprocessed = true;
 	}
-	return 0;
-	/*
-	// Create datamanager
-	std::pair<std::string,treeTypes> selfilenameTreeType = createdatamanager(*datafiles);
-	std::string selectorfilename = selfilenameTreeType.first;
-	dataType = selfilenameTreeType.second;
-	
-	std::string srcfilename( selectorfilename );
-	srcfilename.replace(srcfilename.find(".h"),3,std::string(".cc")) ;
 
-	std::string command1("mv "+selectorfilename+" "+analysispkgpath+"/interface/"+selectorfilename);
-	std::string command2("mv "+srcfilename+" "+analysispkgpath+"/src/"+srcfilename);
-	
-	system( command1.c_str() );
-	system( command2.c_str() );
-	// Compilation stuff
-	//system("make");
-
-	// Freeing mem
-	if( datafiles != 0)
+	if( ! wasprocessed )
 	{
-		delete datafiles;
+		std::cerr << "\033[1;31mdatamanagercreator ERROR\033[1;m: Could not find dataset '" 
+			<< dataName << "' in the list of known datasets. You should update this code..." << std::endl;
+		return -2;
 	}
 
-	return 0;*/
+	return 0;
 }
