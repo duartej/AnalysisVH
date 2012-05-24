88ed464560f6048f722bfe5c89001e8572ad7462
duarte <duarte@gridui.ifca.es>
2012-05-24 21:58:06 +0200

    Updated with correlated errors. It seems it is working
    Ignore-this: 7db920dfe0e3b57a5b6bf974190be991
    
    darcs-hash:20120524195806-5c877-0aa91118e7b87e33377a3643db5db5cb87d72217.gz

--- Utils/bluemethod.py
+++ Utils/bluemethod.py
@@ -3,24 +3,17 @@
 Combine several unbiassed estimation of an observable by using the
 BLUE tecnique (Best Linear Unbiased Estimate)        
 """
-#-- Global systematics: In relative error
-# --- systematics for fake rate method
-DDMMC = { "mmm": 0.31, "mme": 0.46, "eem": 0.42, "eee": 0.14, "leptonchannel": 0.35 }
-# -- FIXME: Extract the statistical SAMPLESTAT error for WZ and ZZ from the files
-SYS = { "Lumi": 0.022, " TriggerEff": 0.015, "LeptonEff": 0.020, "MuonMomentumScale": 0.015,
-		"ElecEnergyScale": 0.025, "METres": 0.020, "DDMMC": DDMMC, "PILEUP": 0.020,
-		"PDF": 0.014 
-	}
-
 # -- Correspondence between index tuples,list and channel
-CORRLIST = { 0: "eee", 1: "eem", 2: "mme", 3:"mmm" }
-# -- Corrsepondece between matrices
-CORRMATRIX = {}
+# --- VERY IMPORTANT NOT TO CHANGE THE ORDER!
+IDCHANNEL = { 0: "eee", 1: "eem", 2: "mme", 3:"mmm" }
+CHANNELID = dict(map(lambda (x,y): (y,x), IDCHANNEL.iteritems()))
+# -- Correspondence between matrices
+IDCHANNELMATRIX = {}
 idmat = 0
 # Assuming return of .keys is ordered
-for id in CORRLIST.keys():
-	for idother in CORRLIST.keys():
-		CORRMATRIX[idmat] = (CORRLIST[id],CORRLIST[idother])
+for id in IDCHANNEL.keys():
+	for idother in IDCHANNEL.keys():
+		IDCHANNELMATRIX[idmat] = (IDCHANNEL[id],IDCHANNEL[idother])
 		idmat += 1
 
 
@@ -43,128 +36,168 @@ def checkenv():
 				raise RuntimeError(message )
 
 
-def extractyields(asciirawtable,titlerow):
-	"""..function::extractyields(asciirawtable,titlerow) --> (n,nerrorstat)
+def geterrorarray(xs,xserrors):
+	""".. function:: geterrorarray(xs,xserrors) --> (E,Esys)
+
+	Calculate the arrays (to fill later the matrices) of absolute errors and returns
+	two objects, a dictionary with errors separated into sys, stat and lumi (plus the total of them)
+	and another dictionary where all the errors are separated by its sources; this is done
+	just to keep separatly this information if it is needed at the future. 
+	Also, the arrays are calculated taking into account the correlations
+
+	:param xs: dictionary of cross-section values splitted in channels
+	:type xs: dict(str:float)
+	:param xserrors: dictionary of cross-section relative errors splitted in sources of error 
+	                 an in channels. See help in funcitionspool.getxserrorsrel
+	:type xserrors: dict(str:dict(str:float))
+
+	:return: absolute errors, just splitted in stat,sys,lumi and total in the first element
+	         of the tuple, and splitted by source of error for the second one
+	:rtype: (dict(str:array(float)),dict(srt:array(float)))
+	"""
+	from array import array
+	# ========= ERROR MATRIX INITIALIZATION =====================
+	# -- Separate the error array into three contributions: sys,lumi,stat
+	#    plus the total of them
+	arrayE = dict([(itype,array('d',[0.0 for k in IDCHANNELMATRIX.keys()])) \
+			for itype in ["sys","lumi","stat","total"]])
+	# Special dictionary of arrays-matrix to separate each systematic error
+	arrayEsys = dict([(itype,array('d',[0.0 for k in IDCHANNELMATRIX.keys()])) \
+			                        for itype in xserrors.keys()])
 	
-	From a table in .tex format (the reduced one of the standard output), the yields of the
-	'titlerow' row will be extracted
+	# Note that the asymetric errors are taken as mean
+	evalsyserr = lambda sysname,lchannel,rchannel: ((xserrors[sysname][lchannel][0]*xs[lchannel])*(xserrors[sysname][rchannel][0]*xs[rchannel])+\
+			(xserrors[sysname][lchannel][1]*xs[lchannel])*(xserrors[sysname][rchannel][1]*xs[rchannel]))/2.0
 
-	:param asciirawtable: lines in the .tex reduced table output from the standard analysis chain
-	:type asciirawtable: str
-	:param titlerow: the name of the sample or of the column/row you want to extract the yields
-	:type titlerow: str
+	# -- CORRELATIONS: DECIDING WHAT IS CORRELATED
+	#    We say two systematics are fully correlated between two channels when the variation of the error in one channel 
+	#    is the same as the variation of the error of the other channel
+	for id,(lval,rval) in IDCHANNELMATRIX.iteritems():
+		# - 1. Lepton eff. - Asumming fully correlation between channels 
+		arrayEsys["LeptonEff"][id] = evalsyserr("LeptonEff",lval,rval)
+		# - 2. Trigger eff.: fully correlated when Z->ll channels, i.e., eee with eee and eem (and viceversa)
+		#                    and mmm with mmm and mme. 
+		if lval[:-1] == rval[:-1]:
+			arrayEsys["TriggerEff"][id] = evalsyserr("TriggerEff",lval,rval)
+		# - 3. Electron Energy Scale: assuming fully correlation between electronic channels (contains at least an electron)
+		if lval.find('e') != -1 and rval.find('e') != -1:
+			arrayEsys["ElecEnergyScale"][id] = evalsyserr("ElecEnergyScale",lval,rval)
+		# - 4. Muon Momentum Scale: assuming fully correlation between muonic channels (contains at least an muon)
+		if lval.find('m') != -1 and rval.find('m') != -1:
+			arrayEsys["MuonMomentumScale"][id] = evalsyserr("MuonMomentumScale",lval,rval)
+		# - 5. Pile-up reweighting technique: assuming fully correlation between all channels
+		arrayEsys["PILEUP"][id] = evalsyserr("PILEUP",lval,rval)
+		# - 6. PDF sytematic effect: assuming fully correlation between all channels
+		arrayEsys["PDF"][id] = evalsyserr("PDF",lval,rval)
+		# - 7. MET resolution effect: assuming fully correlation between all channels
+		arrayEsys["METres"][id] = evalsyserr("METres",lval,rval)
+		# - 8. Fakeable object method: assuming not correlation between channels
+		if lval == rval:
+			arrayEsys["DDMMC"][id] = evalsyserr("DDMMC",lval,rval)
+		# - 9. Luminosity effect: assuming fully corration between all channels
+		arrayEsys["Lumi"][id] = evalsyserr("Lumi",lval,rval)
+		# - 10.Statistics, not correlated
+		if lval == rval:
+			# -- Signal
+			arrayEsys["STATS"][id] = evalsyserr("STATS",lval,rval)
+			# -- WZ
+			arrayEsys["Stat"][id] = evalsyserr("Stat",lval,rval)
+	
+	# Per contributions array
+	for errorname,arrayvalues in arrayEsys.iteritems():
+		whereitgoes = ""
+		if errorname == "STATS":
+			whereitgoes="stat"
+		elif errorname == "Lumi":
+			whereitgoes="lumi"
+		else:
+			whereitgoes="sys"
+		for id in xrange(len(arrayvalues)):
+			arrayE[whereitgoes][id] += arrayvalues[id]
 
-	:return: the yields and the statistical error
-	:rtype: tuple(float,float)
+	# Finally filling the total
+	for id in xrange(len(arrayvalues)):
+		for kinderr in filter(lambda x: x != "total",arrayE.keys()):
+			arrayE["total"][id] += arrayE[kinderr][id]
+
+	return arrayE,arrayEsys
+
+
+
+def bluemethod(workingpath,zoutrange,whatuse,verbose):
+	""" ..function:: bluemethod(workingpath,zoutrange,whatuse) --> 
 	"""
-	line=None
-	try:
-		line = filter(lambda x: x.find(titlerow) != -1,asciirawtable)[0]
-	except IndexError:
-		raise RuntimeError("\033[31;1mextractyields ERROR\033[m Not found the sample '%s'" % titlerow)
-	N = float(line.split("&")[-1].split("$\\pm$")[0].strip())
-	Nerrstat = float(line.split("&")[-1].split("$\\pm$")[1].strip())
-
-	return (N,Nerrstat)
-
-
-def bluemethod(workingpath,zoutrange,whatuse):
-	"""
-	"""
-	from xscalc import xscalc 
 	from array import array
 	from math import sqrt
+	from functionspool_mod import getxserrorsrel
 	import ROOT
-	# Set-up systematics
-	corrsys = [ "TriggerEff", "LeptonEff", "PILEUP", "PDF" ]
-	muoncorrsys = corrsys+["MuonMomentumScale"]
-	eleccorrsys = corrsys+["ElecMomentumScale"]
-
-	howchannels = len(CORRLIST)
 	
-	# Extract cross-section and errors
-	xsdict = xscalc(workingpath,zoutrange,None)
-	# -- Using the inclusive or exclusive?
-	#whatuse = "WZinclusive"
-	xs = dict(map(lambda (channel,xsd): (channel,xsd[whatuse]),filter(lambda (channel,xsd): channel != "leptonchannel",xsdict.iteritems())))
-	# xs = { 'channel': (value,stat,sys_up,sys_down,lumi), ... }
-	
-	# Getting the indexs of the elements of the diagonal
-	iddiagonal = filter( lambda x: (x % (len(CORRLIST)+1)) == 0, CORRMATRIX.keys())
-	
-	weights = dict(map(lambda channel: (channel,0.0),xs.keys()))
-	# -- Loop for minimisation
-	# NOTA: si he d'utilitzar la seccio eficaz nova, llavors haig de convertir els valors trobats dels errors de valors 
-	#       absoluts a valors relatius (dividint per la xs corresponent) i tornant a convertir-los a valors absoluts fent
-	#       servir la nova xs  ----> 
-	combined_meas_old = 1
-	combined_meas_this= 200
-	while( abs(combined_meas_this-combined_meas_old) > 0.01 ):
-		print "****************** NEW Iteration **********************"
-		# Initialize the content of the errors
-		arraysys = {}
-		for err in [ "stat", "sys", "lumi"]:
-			arraysys[err] = array('d',[ 0.0 for i in xrange(len(CORRMATRIX)) ])
-		for id,(lval,rval) in CORRMATRIX.iteritems():
-			# Systematics correlated??
-			arraysys["sys"][id] = xs[lval][2]*xs[rval][2]
-			# Not correlated
-			if id in iddiagonal:
-				arraysys["stat"][id] = xs[lval][1]*xs[rval][1]
-				arraysys["lumi"][id] = xs[lval][4]*xs[rval][4]
-		matrixerr = {}
-		matrixerr_inv = {}
-		for err in [ "stat", "sys", "lumi" ]: 
-			matrixerr[err] = ROOT.TMatrixD(howchannels,howchannels)
-			matrixerr[err].SetMatrixArray(arraysys[err])
-			matrixerr_inv[err] = ROOT.TMatrixD(howchannels,howchannels)
-			matrixerr_inv[err].SetMatrixArray(arraysys[err])
-		matrixerr["total"] = matrixerr["stat"]+matrixerr["sys"]+matrixerr["lumi"]
-		matrixerr_inv["total"] = matrixerr_inv["stat"]+matrixerr_inv["sys"]+matrixerr_inv["lumi"]
-		matrixerr_inv["total"].Invert()
-		# Up to here-- all the matrices built
-		U = array('d',[ 1.0 for i in xrange(howchannels) ] )
-		Unit = ROOT.TMatrixD(len(U),1)
-		Unit.SetMatrixArray(U)
-		Unit_T = ROOT.TMatrixD(1,len(U))
-		Unit_T.SetMatrixArray(U)
-		# Build the weights
-		# -- Note R is a number
-		R = Unit_T*matrixerr_inv["total"]*Unit
-		IR = 1.0/R(0,0)
-		weightMatrix = matrixerr_inv["total"]*Unit
-		weightMatrix *= IR
-		# And the variance, i.e. sigma**2
-		weightMatrix_T = ROOT.TMatrixD(weightMatrix.GetNcols(),weightMatrix.GetNrows())
-		weightMatrix_T.Transpose(weightMatrix)
-		variance = weightMatrix_T*matrixerr["total"]*weightMatrix
-		# -- Final value
-		xscombined = 0.0
-		for id,channel in CORRLIST.iteritems():
-			xscombined += weightMatrix(id,0)*xs[channel][0]
+	nchannels = len(IDCHANNEL.keys())
+	# Get cross-section and its relative-errors
+	xs,xserrors = getxserrorsrel(workingpath,xstype=whatuse)
+	### ========== INITIALIZATIONS =============================
+	### Arrays to be used to fill the matrices: arrayName  
+	### --- U: link between observable and measure 
+	arrayU = array('d',[1.0 for i in xrange(nchannels)])
+	U = ROOT.TMatrixD(nchannels,1)
+	U.SetMatrixArray(arrayU)
+	U_T = ROOT.TMatrixD(1,nchannels)
+	U_T.SetMatrixArray(arrayU)
+	### --- E: measure covariance 
+	arrayEmain,arrayEsep = geterrorarray(xs,xserrors)
+	E = ROOT.TMatrixD(nchannels,nchannels)
+	E.SetMatrixArray(arrayEmain["total"])
+	E_inv = ROOT.TMatrixD(nchannels,nchannels)
+	E_inv.SetMatrixArray(arrayEmain["total"])
+	E_inv.Invert()
+	### --- w: weights ==> xs_mean = Sum_{i} w_i*xs_i
+	arrayW = array('d', [0.0 for i in xrange(nchannels)])
+	W = ROOT.TMatrixD(nchannels,1)
+	W.SetMatrixArray(arrayW)
+	W_T = ROOT.TMatrixD(1,nchannels)
+	### --- xs: cross-section measurements
+	arrayXS= array('d', [xs[IDCHANNEL[i]] for i in xrange(nchannels)])
+	XS = ROOT.TMatrixD(nchannels,1)
+	XS.SetMatrixArray(arrayXS)
+	XS_T = ROOT.TMatrixD(1,nchannels)
+	XS_T.Transpose(XS)
+	# ===== END INITIALIZATION
 
-		# Extract the total error (separated by stat, sys and lumi)
-		#MXSec = ROOT.TMatrixD(howchannels,1)
-		denom = 1.0/variance(0,0)
-		# Total errors
-		total = { "sys": 0.0, "stat": 0.0, "lumi":0.0 }
-		for i in xrange(howchannels):
-			for m in xrange(howchannels):
-				for n in xrange(howchannels):
-					for j in xrange(howchannels):
-						for errtype in total.iterkeys():
-							total[errtype] += matrixerr_inv["total"](i,m)*matrixerr[errtype](m,n)*matrixerr_inv["total"](n,j)
+	# Extract the weights (using the BLUE method)
+	kinv = (U_T*E_inv*U)(0,0)
+	k = 1.0/kinv
+	W = E_inv*U
+	W *= k
+	W_T.Transpose(W)
 
-		errors = dict( map(lambda (errtype,val): (errtype,sqrt(val)/denom), total.iteritems()) )
-		weightmessage = "+++ Weights:  "
-		for id,channel in CORRLIST.iteritems():
-			weightmessage += ("%s: %.4f  " % (channel,weightMatrix(id,0)))
-		print weightmessage
-		print "+++ xs_{%s}=%.4f+-%.4f(stat)+-%.4f(sys)+-%.4f(lumi)   total_err=%.4f" % (whatuse,xscombined,errors["stat"],errors["sys"],errors["lumi"],sqrt(variance(0,0)))
-		combined_meas_old = combined_meas_this
-		combined_meas_this= xscombined
-		#print combined_meas_old,combined_meas_this
-		
+	# Get the estimated cross-section
+	xsmean = (XS_T*W)(0,0)
+	S = 0.0
+	# Estimating the validity of the estimator
+	for i in xrange(nchannels):
+		for j in xrange(nchannels):
+			S += (xsmean-XS(i,0))*E_inv(i,j)*(xsmean-XS(j,0))
+	# variance separated by error type
+	E_types = {}
+	E_types_inv = {}
+	for errname,arrayerr in arrayEmain.iteritems():
+		E_types[errname] = ROOT.TMatrixD(nchannels,nchannels)
+		E_types[errname].SetMatrixArray(arrayerr)
+
+	variances = {}
+	for errname,Ematrix in E_types.iteritems():
+		variances[errname] = (W_T*Ematrix*W)(0,0)
+	errors = dict(map(lambda (key,x): (key,sqrt(x)), variances.iteritems()))
+
+	if verbose:
+		newline = "\033[32;2mbluemethod VERBOSE\033[m "
+		message = newline+"WEIGHTS:: " 
+		for i,channel in IDCHANNEL.iteritems():
+			message += "%s:%.4f " % (channel,W(i,0))
+		# Matrices: E y E_sys
+		print message
+	
+	return xsmean,errors
 
 
 if __name__ == '__main__':
@@ -173,7 +206,7 @@ if __name__ == '__main__':
 	
 	#Opciones de entrada
 	parser = OptionParser()
-	parser.set_defaults(workingpath=os.getcwd(),zoutrange=False,xstype="WZexclusive")
+	parser.set_defaults(workingpath=os.getcwd(),zoutrange=False,xstype="exclusive",verbose=False)
 	parser.add_option( '-w', '--workingdir', action='store', type='string', dest='workingpath',\
 			help="Working directory. It must exist the usual folder structure")
 	parser.add_option( '-z', '--ZrangeasinMC', action='store_true', dest='zoutrange',\
@@ -184,14 +217,23 @@ if __name__ == '__main__':
 	parser.add_option( '-x', '--xs', action='store', dest='xstype',\
 			help="The type of cross-section to be combined. Valid keywords: 'WZinclusive' 'WZexclusive'."\
 			" [Default 'exclusive']")
+	parser.add_option( '-v', '--verbose', action='store_true', dest='verbose',\
+			help="Activing the flag more information is printed [Default: False]")
 	
 	(opt,args) = parser.parse_args()
 
-	if opt.xstype != "WZexclusive" and opt.xstype != "WZinclusive":
+	if opt.xstype != "exclusive" and opt.xstype != "inclusive":
 		message= "\033[31mbluemethod ERROR\033[m Not valid argument for the '-x' option. Valid arguments:"\
 				" 'WZexclusive' 'WZinclusive'"
 		raise RuntimeError(message)
 
 	print "\033[34mbluemethod INFO\033[m Combining the %s cross-section at '%s'" % (opt.xstype,opt.workingpath)
-	bluemethod(opt.workingpath,opt.zoutrange,opt.xstype)
+	xsmean,xserrors = bluemethod(opt.workingpath,opt.zoutrange,opt.xstype,opt.verbose)
+
+	output = "xs=%.4f" % xsmean
+	for errname,val in filter(lambda (x,y): x!="total",xserrors.iteritems()):
+		output+= "+-%.4f(%s)" % (val,errname)
+	output += "  (total:%.4f)" % xserrors["total"]
+
+	print output
 
