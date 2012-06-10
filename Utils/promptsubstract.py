ba82c98a04e1cc123a3cb4874089a07b0d9b9a78
duarte <duarte@gridui.ifca.es>
2012-06-10 14:31:59 +0200

    Added the extractall function to tarfile (included from python 2.5.. I don't know why is failing at 10-june)
    Ignore-this: 70de06bfda622c2e59e661a392d2cf67
    
    darcs-hash:20120610123159-5c877-494c8172f6e61301fdf6aca9fce4ce98b3c34008.gz

--- Utils/promptsubstract.py
+++ Utils/promptsubstract.py
@@ -108,6 +108,49 @@ def substractprompt(filefakes,filepromptsdict, outfilename, lumi=None):
 
 	return yieldsppp
 
+# Just copied from tarfile.py (if we are below 2.5 python)
+def extractall(self, path=".", members=None):
+        """Extract all members from the archive to the current working
+           directory and set owner, modification time and permissions on
+           directories afterwards. `path' specifies a different directory
+           to extract to. `members' is optional and must be a subset of the
+           list returned by getmembers().
+        """
+	import os
+	import copy
+	import operator
+
+        directories = []
+
+        if members is None:
+		members = self
+		
+	for tarinfo in members:
+		if tarinfo.isdir():
+			# Extract directories with a safe mode.
+			directories.append(tarinfo)
+			tarinfo = copy.copy(tarinfo)
+			tarinfo.mode = 0700
+		self.extract(tarinfo, path)
+		
+	# Reverse sort directories.
+	directories.sort(key=operator.attrgetter('name'))
+	directories.reverse()
+
+        # Set correct owner, mtime and filemode on directories.
+	for tarinfo in directories:
+		dirpath = os.path.join(path, tarinfo.name)
+		try:
+			self.chown(tarinfo, dirpath)
+			self.utime(tarinfo, dirpath)
+			self.chmod(tarinfo, dirpath)
+		except ExtractError, e:
+			if self.errorlevel > 1:
+				raise
+			else:
+				self._dbg(1, "tarfile: %s" % e)
+
+
 if __name__ == '__main__':
 	import sys
 	import os
@@ -175,7 +218,11 @@ a loop for each channel subfolder."""
 				# Recovering the original samples
 				shutil.rmtree("cluster_Fakes")
 				tar = tarfile.open("fakespool.tar.gz")
-				tar.extractall()
+				try:
+					tar.extractall()
+				except AttributeError:
+					# By-passing the python2.4
+					extractall(tar)
 				tar.close()
 				if opt.force:
 					os.remove("fakespool.tar.gz")
