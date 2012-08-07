2b4f4610e259e6aadcbb1f8823fc0cd4c8a2d2db
Jordi Duarte Campderros <duarte@ifca.unican.es>
2012-08-07 16:14:25 +0200

    Just added missing bins

--- PUWeight/src/PUWeight.cc
+++ PUWeight/src/PUWeight.cc
@@ -219,12 +219,13 @@ TH1F* PUWeight::IdealMCHistogram(EMCDistribution mcdistr) {
 #endif
 
   fMC = new TH1F("PU_MC", "PU^{MC} Weight", ndbins, xmin, xmax);
-  double bins[50] = {
+  double bins[60] = {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     10,11,12,13,14,15,16,17,18,19,
     20,21,22,23,24,25,26,27,28,29,
     30,31,32,33,34,35,36,37,38,39,
-    40,41,42,43,44,45,46,47,48,49
+    40,41,42,43,44,45,46,47,48,49,
+    50,51,52,53,54,55,56,57,58,59
   };
 
   unsigned int nbins = 25;
